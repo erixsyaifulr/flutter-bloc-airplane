975a13718b2e0b3fcad43c2a7eb8e82a9925c6f5
erix_syaifulr <erix.rohman@lidahbuayagroup.com>
2022-07-28 21:12:25 +0700

    adding new route

--- lib/shared/routes.dart
+++ lib/shared/routes.dart
@@ -1,4 +1,7 @@
+import 'package:airplane/ui/pages/bonus/bonus_page.dart';
 import 'package:airplane/ui/pages/get_started/get_started_page.dart';
+import 'package:airplane/ui/pages/main_page/main_page.dart';
+import 'package:airplane/ui/pages/sign_up/sign_up_page.dart';
 import 'package:airplane/ui/pages/splash_screen/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -8,9 +11,15 @@ class Routes {
   //static variables
   static const String index = '/';
   static const String getStarted = '/get-started';
+  static const String signUp = '/sign-up';
+  static const String bonus = '/bonus';
+  static const String main = '/main';
 
   static final routes = <String, WidgetBuilder>{
     index: (BuildContext context) => const SplashPage(),
     getStarted: (BuildContext context) => const GetStartedPage(),
+    signUp: (BuildContext context) => const SignUpPage(),
+    bonus: (BuildContext context) => const BonusPage(),
+    main: (BuildContext context) => const MainPage(),
   };
 }
