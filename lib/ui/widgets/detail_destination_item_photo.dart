08cc48c6c00a7f5ad54bb1ba1f8d4df15b258f4a
erix_syaifulr <erix.rohman@lidahbuayagroup.com>
2022-07-28 21:16:53 +0700

    create reusable widget for detail image item

---
+++ lib/ui/widgets/detail_destination_item_photo.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DetailDestinationItemPhoto extends StatelessWidget {
+  final String imageUrl;
+  const DetailDestinationItemPhoto({
+    Key? key,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      margin: const EdgeInsets.only(right: 15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(imageUrl),
+        ),
+      ),
+    );
+  }
+}
